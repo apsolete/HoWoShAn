d05aa1bc4ea934c6ecd33823a49b12bc3a62e8e3
qelveen <qelveen@gmail.com>
2020-02-26 10:32:23 +0300

    Clean the code

--- app/src/main/java/com/apsolete/myworkshop/common/CustomFragment.java
+++ app/src/main/java/com/apsolete/myworkshop/common/CustomFragment.java
@@ -6,8 +6,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.apsolete.myworkshop.ui.workshop.WorkshopViewModel;
-
 import androidx.annotation.IdRes;
 import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
