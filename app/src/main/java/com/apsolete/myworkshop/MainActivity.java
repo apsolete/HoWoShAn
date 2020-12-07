b6014774db53e0cf458565dd4af3043636d52ca7
qelveen <qelveen@gmail.com>
2020-12-07 12:26:26 +0300

    Fixed deprecated code

--- app/src/main/java/com/apsolete/myworkshop/MainActivity.java
+++ app/src/main/java/com/apsolete/myworkshop/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_workshop, R.id.nav_equipment, R.id.nav_tools, R.id.nav_materials, R.id.nav_hardware, R.id.nav_send)
-                .setDrawerLayout(drawer)
+                .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
