5f671646cef9fd1dd702c1428ae514e99ff74b45
qelveen <qelveen@gmail.com>
2021-03-21 00:00:46 +0300

    Updated TypesFragment

--- app/src/main/java/com/apsolete/workshop/ui/equipment/types/TypesFragment.java
+++ app/src/main/java/com/apsolete/workshop/ui/equipment/types/TypesFragment.java
@@ -1,15 +1,29 @@
 package com.apsolete.workshop.ui.equipment.types;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.apsolete.customui.Adapters;
 import com.apsolete.customui.CustomFragment;
+import com.apsolete.customui.ScrollChildSwipeRefreshLayout;
 import com.apsolete.workshop.R;
 import com.apsolete.workshop.db.entities.EquipType;
 
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.Observer;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
 public class TypesFragment extends CustomFragment<TypesViewModel>
 {
+    private TypesAdapter mTypesAdapter;
+
     public TypesFragment()
     {
         super(R.layout.fragment_equipmenttypes, R.string.title_equipmenttypes, TypesViewModel.class);
@@ -21,12 +35,67 @@ public class TypesFragment extends CustomFragment<TypesViewModel>
         return mViewModel;
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        mTypesAdapter = new TypesAdapter();
+        mViewModel.getEquipTypes().observe(this, new Observer<List<EquipType>>()
+        {
+            @Override
+            public void onChanged(List<EquipType> equipTypes)
+            {
+                mTypesAdapter.setList(equipTypes);
+            }
+        });
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View root = super.onCreateView(inflater, container, savedInstanceState);
+        ListView listView = root.findViewById(R.id.list_types);
+        listView.setAdapter(mTypesAdapter);
+
+        // Set up progress indicator
+        final ScrollChildSwipeRefreshLayout swipeRefreshLayout = root.findViewById(R.id.refresh_layout);
+        swipeRefreshLayout.setColorSchemeColors(
+                ContextCompat.getColor(getActivity(), R.color.colorPrimary),
+                ContextCompat.getColor(getActivity(), R.color.colorAccent),
+                ContextCompat.getColor(getActivity(), R.color.colorPrimaryDark));
+        // Set the scrolling view in the custom SwipeRefreshLayout.
+        swipeRefreshLayout.setScrollUpChild(listView);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener()
+        {
+            @Override
+            public void onRefresh()
+            {
+                mViewModel.load();
+            }
+        });
+
+        return root;
+    }
+
     private static class TypesAdapter extends Adapters.ItemsAdapter<EquipType>
     {
         @Override
         public View getView(int i, View view, ViewGroup viewGroup)
         {
-            return null;
+            View rowView = view;
+            if (rowView == null) {
+                LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+                rowView = inflater.inflate(R.layout.item_equipmenttype, viewGroup, false);
+            }
+//            EquipType equipType = getItem(i);
+//
+//            TextView textTypeId = (TextView) rowView.findViewById(R.id.text_equipmenttype_id);
+//            textTypeId.setText(Long.toString(equipType.id));
+//
+//            TextView textTypeName = (TextView) rowView.findViewById(R.id.text_equipmenttype_name);
+//            textTypeId.setText(equipType.name);
+
+            return rowView;
         }
     }
 }
--- app/src/main/java/com/apsolete/workshop/ui/equipment/types/TypesViewModel.java
+++ app/src/main/java/com/apsolete/workshop/ui/equipment/types/TypesViewModel.java
@@ -1,11 +1,30 @@
 package com.apsolete.workshop.ui.equipment.types;
 
 import android.app.Application;
-import androidx.annotation.NonNull;
+
 import com.apsolete.customui.CustomViewModel;
+import com.apsolete.workshop.db.entities.EquipType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
 public class TypesViewModel extends CustomViewModel
 {
+    private MutableLiveData<List<EquipType>> mEquipTypes;
+    public LiveData<List<EquipType>> getEquipTypes()
+    {
+        if (mEquipTypes == null)
+        {
+            mEquipTypes = new MutableLiveData<>();
+            load();
+        }
+        return mEquipTypes;
+    }
+
     public TypesViewModel(@NonNull Application application)
     {
         super(application);
@@ -15,5 +34,11 @@ public class TypesViewModel extends CustomViewModel
     public void start()
     {
 
+    }
+
+    //@Override
+    public void load()
+    {
+        mEquipTypes.setValue(new ArrayList<EquipType>());
     }
 }
